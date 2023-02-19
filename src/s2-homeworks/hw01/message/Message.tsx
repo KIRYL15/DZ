35d176046d5d7a8f2f04c86391ebfc5484e1fa7c
Kirill Pravdin <kirill-pravdin@mail.ru>
2023-02-19 23:03:47 +0300

    feat(HW1):correcting-3 errors after the first check - name editing HW1

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -30,3 +30,32 @@ export const Message = (props: MessagePropsType) => {
         </div>
     )
 }
+
+// <div id={'hw1-message-' + props.message.id}
+//      className={s.message}>
+//
+//     {/*IMAGE*/}
+//     <div className={s.imageAndText}>
+//         <img className={s.img}
+//              id={'hw1-avatar-' + props.message.id}
+//              src={props.message.user.avatar}/>
+//
+//         {/*TEXT*/}
+//         <div className={s.text}>
+//             <div id={'hw1-name-' + props.message.id} className={s.name}>
+//                 <p>{props.message.user.name}</p>
+//             </div>
+//
+//             <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+//                         {/*создаёт студент*/}
+//                 {props.message.message.text}
+//                 {/**/}
+//                     </pre>
+//
+//             <div id={'hw1-time-' + props.message.id} className={s.time}>
+//                 {props.message.message.time}
+//             </div>
+//
+//         </div>
+//     </div>
+// </div>

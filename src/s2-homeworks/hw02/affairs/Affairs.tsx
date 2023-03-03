d2d5e0828d26f6e6eae655984ca3c7ae66b8d956
Kirill Pravdin <kirill-pravdin@mail.ru>
2023-03-03 20:35:11 +0300

    feat(HW2):correcting-1

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import Affair from './affair/Affair'
+import {Affair} from './affair/Affair'
 import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
-    filter: FilterType
+    data: AffairType[], // need to fix any
+    setFilter: (filter:FilterType)=>void,
+    deleteAffairCallback: (_id:number)=>void,
+    filter: FilterType,
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all')
+        // need to fixprops.setFilter('ALL')
     }
     const setHigh = () => {
+        props.setFilter('high')
         // need to fix
     }
     const setMiddle = () => {
+        props.setFilter('middle')
         // need to fix
     }
     const setLow = () => {
+        props.setFilter('low')
         // need to fix
     }
 
